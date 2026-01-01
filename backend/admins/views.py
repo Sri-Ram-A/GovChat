ac2861c451936a8155c1257d67bb347793f97ad2
Sri-Ram-A <srirnsametalmart@gmail.com>
2026-01-01 21:22:19 +0530

    Admin and Citizen Authentication done with JWT

--- backend/admins/views.py
+++ backend/admins/views.py
@@ -1,11 +1,41 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from rest_framework.permissions import AllowAny,IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
+
 from entities.admins import AdminProfile
-from serializer.admins import AdminProfileSerializer
+import  serializer.admins as admin_serializer
+import serializer.base as base_serializer
+from loguru import logger
 
 class AdminListAPIView(APIView):
+    # permission_classes = [IsAuthenticated]
     def get(self, request):
         admins = (AdminProfile.objects.select_related('user', 'department').all())
-        serializer = AdminProfileSerializer(admins, many=True)
+        serializer = admin_serializer.AdminProfileSerializer(admins, many=True)
         return Response(serializer.data)
+
+class CitizenRegistrationAPIView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = admin_serializer.AdminRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            admin = serializer.save()
+            return Response({
+                "message": "Registration successful",
+            }, status=status.HTTP_201_CREATED)
+        logger.debug(serializer.errors)
+        return Response({"message": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+class CitizenLoginAPIView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = base_serializer.UserLoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+        refresh = RefreshToken.for_user(user)
+        return Response({"access": str(refresh.access_token),"refresh": str(refresh),}, status=status.HTTP_200_OK)
+
--- backend/serializer/admins.py
+++ backend/serializer/admins.py
@@ -1,14 +1,29 @@
 from rest_framework import serializers
-from django.contrib.auth import get_user_model
 
 from entities.admins import AdminProfile
-from .base import UserAllSerializer
 from .governance import DepartmentSerializer
+from .base import UserAllSerializer , UserRegisterSerializer
 
 class AdminProfileSerializer(serializers.ModelSerializer):
     user = UserAllSerializer(read_only=True)
     department = DepartmentSerializer(read_only=True)
-
     class Meta:
         model = AdminProfile
         fields = '__all__'
+
+class AdminRegistrationSerializer(serializers.ModelSerializer):
+    '''This serializer expects a field called user, and that field is a nested object that must follow UserRegisterSerializer.'''
+    
+    user = UserRegisterSerializer()
+    class Meta:
+        model = AdminProfile
+        fields = '__all__'
+
+    def create(self, validated_data):
+        user_data = validated_data.pop('user')
+        user_serializer = UserRegisterSerializer(data=user_data)
+        user_serializer.is_valid(raise_exception=True)
+        user = user_serializer.save()
+        citizen_profile = AdminProfile.objects.create(user=user,**validated_data)
+        return citizen_profile
+
